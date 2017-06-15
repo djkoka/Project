a69ee95d1e19f428500eea05fd6a376bb0042568
Koka <zuka321@gmail.com>
2017-06-15 13:51:07 +0300

    commit

---
+++ src/oop/service/Service.java
@@ -0,0 +1,27 @@
+package oop.service;
+
+
+import oop.model.Device;
+
+/**The interface Service describe list of services of Inventory component
+ *
+ * Created by makovetskyi on 05.10.2016.
+ */
+public interface Service {
+
+    /**
+     * Sort array of Device-s by identification number.
+     *
+     * @param devices - array of Device-s, that need to be sorted
+     */
+    void sortByIN(Device[] devices);
+
+    /**
+     * Filtrate array of Device-s by type
+     *
+     * @param devices - array of Device-s, that need to be filtrated
+     * @param type - type of Devices, that will remain in the array after filtering
+     */
+    void filtrateByType(Device[] devices, String type);
+    
+}
