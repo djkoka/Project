d11ed6eea44c4bc98bbd8190d34be9b77b43d0d2
Nata <zuka321@gmail.com>
2017-06-29 00:41:44 +0300

    commit

--- src/com/netcracker/edu/inventory/service/impl/ServiceImpl.java
+++ src/com/netcracker/edu/inventory/service/impl/ServiceImpl.java
@@ -11,7 +11,7 @@ public class ServiceImpl implements Service {
     @Override
     public void sortByIN(Device[] devices) {
         int calNull = 0;
-        int [] arr = new int[devices.length];
+        int[] arr = new int[devices.length];
         for (int i = 0; i < devices.length; i++) {
             if (devices[i] == null) {
                 calNull++;
@@ -34,9 +34,21 @@ public class ServiceImpl implements Service {
 
     @Override
     public void filtrateByType(Device[] devices, String type) {
+        if (type == null) {
+            for (int i = 0; i < devices.length; i++) {
+                if (devices[i] != null && devices[i].getType() != null) {
+                    devices[i] = null;
+                }
+            }
+            return;
+        }
         for (int i = 0; i < devices.length; i++) {
-            if(devices[i].getType().compareTo(type) != 0){
-                devices[i] = null;
+            if (devices[i] != null) {
+                if (devices[i].getType() == null)
+                    devices[i] = null;
+                else if (!devices[i].getType().equals(type) ) {
+                    devices[i] = null;
+                }
             }
         }
     }
